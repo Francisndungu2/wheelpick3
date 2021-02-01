18844c38691b2fff8344b5c289973acd457de1de
Francisndungu2 <francianmbugua@gmail.com>
2021-02-01 13:19:18 +0300

    adddashboard.java

---
+++ app/src/main/java/com/demotxt/myapp/wheelpick3/DashboardActivity.java
@@ -0,0 +1,37 @@
+package com.demotxt.myapp.wheelpick3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+
+public class DashboardActivity extends AppCompatActivity {
+
+    Button btn_signout;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        btn_signout = findViewById(R.id.signoutBtn);
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+            }
+        });
+
+
+    }
+}
